2f2fe1dfd4a5cd0060f24f9656cb5a98b2191e9d
James <jameswmartin4@gmail.com>
2024-06-23 18:44:00 +0100

    new riot api so refactoring code

--- commands/Interacting with APIs/Refactor LoL Code.js
+++ commands/Interacting with APIs/Refactor LoL Code.js
@@ -34,8 +34,6 @@ async function fetchData(playerName, tagline){
         const response3 = await fetch(riotAPILeagueV4BySummoner + playerID + '?api_key=' + riotAPIToken)
         let playerData = await response3.json();
         console.log(playerData)
-    })
-}
 
         // Ranked Solo Duo Data
         if (Array.isArray(playerData)) {
@@ -79,64 +77,15 @@ async function fetchData(playerName, tagline){
             const Arena = playerData.filter(entry => entry.queueType === 'CHERRY')
             const statsArena = Arena.map(Arena => {
                 const ArenaWR = (Arena.wins / (Arena.wins + Arena.losses) * 100).toFixed(2)
-                const ArenaStatement = (`${playerName} has a ${ArenaWR}% win rate in Arena`)
+                const ArenaStatement = (`${playerName} has a ${ArenaWR}% win rate in Arena.`)
                 console.log(ArenaStatement)
             })
+        }
+    })
 }
-
-/*
-// Process and Display Data
-async function processData(playerData) {
-    if (Array.isArray(playerData)) {
-        const rankedSoloDuo = playerData.filter(entry => entry.queueType === 'RANKED_SOLO_5x5')
-
-        const statements = rankedSoloDuo.map(rankedSoloDuo => { // Filter the data
-            const tier = rankedSoloDuo.tier;
-            const rank = rankedSoloDuo.rank;
-            const leaguePoints = rankedSoloDuo.leaguePoints;
-            const wins = rankedSoloDuo.wins;
-            const losses = rankedSoloDuo.losses;
-            const winRate = (wins / (wins + losses) * 100);
-            const winRateRounded = winRate.toFixed(2);
-            const totalGames = wins + losses;
-            const SoloDuoStatement = (`${playerName} is ${tier} ${rank} ${leaguePoints} LP with a win rate of ${winRateRounded}% in ${totalGames} games in Ranked Solo/Duo.`)
-            console.log(SoloDuoStatement)
-        });
-        resolve(statements);
-    } else {
-        reject(new Error('Rank data is not an array.'));
-    }
-    // Ranked Flex Data
-    if (Array.isArray(playerData)) {
-        const rankedFlex = playerData.filter(entry => entry.queueType === 'RANKED_FLEX_SR')
-        const statementFlex = rankedFlex.map(rankedFlex => {
-            const flexTier = rankedFlex.tier;
-            const flexRank = rankedFlex.rank;
-            const flexLeaguePoints = rankedFlex.leaguePoints;
-            const FlexWRR = (rankedFlex.wins / (rankedFlex.wins + rankedFlex.losses) * 100).toFixed(2)
-            const totalGames = rankedFlex.wins + rankedFlex.losses;
-
-            const FlexStatement = (`${playerName} is ${flexTier} ${flexRank} ${flexLeaguePoints} with a win rate of ${FlexWRR}% in ${totalGames} games in Ranked Flex.`)
-            console.log(FlexStatement)
-        });
-        resolve(statementFlex);
-    } else {
-        reject(new Error('Rank data is not an array.'));
-    }
-    // Arena (internal name is CHERRY??) Data
-    if (Array.isArray(playerData)) {
-        const Arena = playerData.filter(entry => entry.queueType === 'CHERRY')
-        const statsArena = Arena.map(Arena => {
-            const ArenaWR = (Arena.wins / (Arena.wins + Arena.losses) * 100).toFixed(2)
-            const ArenaStatement = (`${playerName} has a ${ArenaWR}% win rate in Arena`)
-            console.log(ArenaStatement)
-        })
-    }
-}
-*/
 
 // Calling a player
-fetchData('pranll 2', 'EUW')
+fetchData('SilkySmoooth', '1126')
 
 module.exports = {
     data: new SlashCommandBuilder()
